cbb679d246e1f270ef666f80cb6bf4a954c4ff24
shreethaar <shreethaara380@gmail.com>
2025-02-13 15:57:43 +0800

    Update hello-world.c

---
+++ 001-fundamentals/hello-world.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    MessageBox(NULL,"Hello, World!","My First Windows App",MB_OK | MB_ICONINFORMATION);
+    return 0;
+}
+
