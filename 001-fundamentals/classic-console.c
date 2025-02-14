67a68b171688a35c68696199d36563a7a357bd75
shreethaar <shreethaara380@gmail.com>
2025-02-15 06:32:07 +0800

    Add classic-console.c

---
+++ 001-fundamentals/classic-console.c
@@ -0,0 +1,15 @@
+/*
+ * Main function prototypes
+ * examples:
+ * int main(void);
+ * int main(int argc, char **argv);
+ * int main(int argc, char *argv[]);
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    puts("This is a classic C program.");
+    return 0;
+}
+
