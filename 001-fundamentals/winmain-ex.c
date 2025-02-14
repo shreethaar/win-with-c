133620db277e96a64dfda2cf0f5c9b638d442074
shreethaar <shreethaara380@gmail.com>
2025-02-15 06:32:23 +0800

    Add winmain-ex.c

---
+++ 001-fundamentals/winmain-ex.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WINAPU wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR szCmdLine, int CmdShow) {
+    MessageBoxW(NULL,szCmdLine,L"Title",MB_OK);
+    return 0;
+}
+
